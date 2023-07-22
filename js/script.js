082df6dc768e46393991d96b2ed22754072eea6d
velthereal <lera2005olex@gmail.com>
2023-07-22 20:43:14 +0300

    added something in js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ let signup_btn = document.querySelector('.signup-btn');
 let signin_btn = document.querySelector('.signin-btn');
 let signin_page = document.querySelector('.signin');
 let signup_page = document.querySelector('.signup');
-let btn = document.querySelector('.btn');
+let btn = document.querySelectorAll('.btn');
+
+let registration_form = document.forms.registration;
+let login_form = document.forms.login;
 
 signup_btn.addEventListener('click', () => {
 	signin_page.classList.add('none');
@@ -12,6 +15,8 @@ signin_btn.addEventListener('click', () => {
 	signin_page.classList.remove('none');
 	signup_page.classList.add('none');
 });
-btn.addEventListener('click', () => {
-	window.location.href = 'page.html';
-})
+for(let i = 0; i < btn.length; i++){
+	btn[i].addEventListener('click', () => {
+		window.location.href = 'page.html';
+	});
+}
